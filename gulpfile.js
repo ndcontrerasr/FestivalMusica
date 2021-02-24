0ffa3549fbac839718d0762ad2987c8717d8045f
ndcontrerasr <https://github.com/ndcontrerasr/Udemy.git>
2021-02-23 20:27:52 -0500

    lineup

--- gulpfile.js
+++ gulpfile.js
@@ -31,14 +31,34 @@
 
 //Compilar SASS con gulp --------------------
 
-const { series, src, dest, watch } = require('gulp');
+const { series, src, dest, watch, parallel } = require('gulp');
 const sass = require('gulp-sass');
 const imagemin = require('gulp-imagemin');
+const notify = require('gulp-notify');
+const plumber = require('gulp-plumber');
+const webp = require('gulp-webp');
 //Funcion que compila SASS
+
+const path = {
+	imagenes: 'src/img/**/*'
+}
 
 function css(done) {
 	console.log('Compilando SASS');
 	return src('src/scss/app.scss')
+		//------------------------------
+		//Mensahe de error en el codigo
+		.pipe(plumber({ // * 3 * //
+			errorHandler: function(err) {
+				notify.onError({ // * 4 * //
+					title:    'Gulp Error',
+					message:  '<%= error.message %>',
+					sound:    'Bottle'
+				})(err);
+				this.emit('end'); // * 5 * //
+			}
+		}))
+		//------------------------------
 		.pipe(sass({
 			outputStyle: 'expanded'
 		}))
@@ -46,15 +66,22 @@ function css(done) {
 }
 
 function imagenes() {
-	return src('src/img/**/*')
+	return src(path.imagenes)
 		.pipe(imagemin() )
-		.pipe(dest('./build/img'))
+		.pipe(dest('build/img'))
+		// .pipe(notify({message: 'Imagenes Minificadas'}));
 }
 
 function watchArchivos() {
 	watch('src/scss/**/*.scss', css);
 }
 
+function versionWebp() {
+	return src(path.imagenes)
+		.pipe(webp())
+		.pipe(dest('build/img'))
+}
+
 exports.css = css;
-exports.imagenes = imagenes;
-exports.watch = watchArchivos;
+exports.imagenes = series(imagenes, versionWebp);
+exports.default = watchArchivos;
