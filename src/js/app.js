5b48162e667e20a444f0cb9f462993fc6309b57c
ndcontrerasr <https://github.com/ndcontrerasr/Udemy.git>
2021-02-26 01:41:35 -0500

    global

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,40 @@
 console.log('Listo!!!');
 
+document.addEventListener('DOMContentLoaded', function() {
+	scrollNav();
+
+	navegacionFija();
+});
+
+function navegacionFija() {
+
+	const barra = document.querySelector('.header');
+
+	//registrar el intresection Observer
+	const observer = new IntersectionObserver( function(entries){
+		if (entries[0].isIntersecting) {
+			barra.classList.remove('fijo');
+		}
+		else {
+			barra.classList.add('fijo');
+		}
+	});
+
+	//elemento a observar
+	observer.observe(document.querySelector('.sobre-festival'));
+}
+
+function scrollNav() {
+	const enlaces = document.querySelectorAll('.principal a');
+
+	enlaces.forEach(function( enlace ) {
+		enlace.addEventListener('click', function(e) {
+			e.preventDefault();
+			const seccion = document.querySelector(e.target.attributes.href.value);
+
+			seccion.scrollIntoView({
+				behavior: 'smooth',
+			});
+		});
+	});
+}
